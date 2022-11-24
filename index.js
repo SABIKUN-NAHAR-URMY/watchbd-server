cba598e71c5124d6dc8a675a5cf6c48b0869847a
SABIKUN-NAHAR-URMY <snurmy18@gmail.com>
2022-11-24 23:08:59 +0600

    database connection done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -7,6 +8,20 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ho0d8c2.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+
+    }
+    finally{
+
+    }
+}
+run().catch(error => console.error(error));
 
 app.get('/', (req, res) => {
     res.send('WatchBD server is running')

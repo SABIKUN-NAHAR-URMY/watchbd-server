06fea3222ac395e34fc6a1bf9da785085aeb67ef
SABIKUN-NAHAR-URMY <snurmy18@gmail.com>
2022-11-25 17:14:38 +0600

    category idwize data loaded

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -13,11 +13,25 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ho0d8c2.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-    try{
-        const watchesCollection = client.db('productCategory').collection('categories');
+async function run() {
+    try {
+        const watchesCategoryCollection = client.db('productCategory').collection('categories');
+        const watchesProductsCollection = client.db('productCategory').collection('products');
+
+        app.get('/category', async (req, res) => {
+            const query = {};
+            const result = await watchesCategoryCollection.find(query).toArray();
+            res.send(result);
+        })
+
+        app.get('/products/:categoryID', async (req, res) => {
+            const id = req.params.categoryID;
+            const query = { category_id: parseInt(id) };
+            const result = await watchesProductsCollection.find(query).toArray();
+            res.send(result);
+        })
     }
-    finally{
+    finally {
 
     }
 }
